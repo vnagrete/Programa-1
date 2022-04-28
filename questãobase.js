797e29ec9f05a9eca41162e4412d4d4996cff3c3
vnagrete <102927668+vnagrete@users.noreply.github.com>
2022-04-28 15:47:40 -0300

    Add files via upload

--- questãobase.js
+++ questãobase.js
@@ -1,61 +1,4 @@
-/*
-Questão 13-40 do Shigley 7ª ed (675)
-A questão informa a engrenagem motora
-O torque do motor: 1200 lbf.in
-A direção da força: Sentido anti horário
-Eixo x positivo para a direita e z positivo para fora
-
-Eixo A
-Engrenagem 2 (motor): 
-Passo diametral (pdn2) = 8 dentes/in
-z2 = 14 dentes
-ângulo de pressão normal (fi2) = 20°
-ângulo de hélice (psi2) = 30°
-helice direita
-
-Eixo B
-Engrenagem 3: 
-Passo diametral (pdn3) = 8 dentes/in
-z3 = 36 dentes
-ângulo de pressão normal (fi3) = 20°
-ângulo de hélice (psi3) = 30°
-helice esquerda
-
-Engrenagem 4: 
-Passo diametral (pdn4) = 5 dentes/in
-z4 = 15 dentes
-ângulo de pressão normal (fi4) = 20°
-ângulo de hélice (psi4) = 15°
-helice esquerda
-
-Eixo C
-Engrenagem 5: 
-Passo diametral (pdn5) = 5 dentes/in
-z5 = 45 dentes
-ângulo de pressão normal (fi5) = 20°
-ângulo de hélice (psi5) = 15°
-helice direita
-
-O eixo A é composto pelos mancais A e B
-da2z = -2 in
-daBz = -4 in
-
-O eixo B é composto pelos mancais C e D
-dc3z = -6.5 in
-dc4z = -3 in
-dcDz = -8 in
-
-O eixo C é composto pelos mancais E e F
-de5z = -3.25 in
-deFz = -6.5 in
-
-Distâncias em Y
-
-da2y = di2/2
-dc3y = di3/2
-dc4y = di4/2
-de5y = di5/2
-*/
+//Questão 13-40 do Shigley 7ª ed (675)
 
 //Deve ser determinada a engrenagem motora do sistema, e seu sentido deve ser determinado, pois a apartir dele será determinado o sentido das demais engrenagens do sistema
 
@@ -186,23 +129,23 @@ AGORA EM DIANTE É IMPORTANTE USAR O ELEMNETO DO VETOR E NÃO A VARIÁVEL DA
 INTENSIDADE DA FORÇA. EXEMPLO: USAR F23[0] NO LUGAR DE F23x*/
 
 //ANÁLISE DOS EIXOS
-//Eixo a (somente uma interação por vez)
+//Eixo a 
 
 let F32 = F23.map(function(i) { return -i}) //Será usada a força de 3 em 2
 
 //Determinação do mancal com carregamento axial
-let LA23 = []
-let LAB = []
+let LM32 = []
+let LMOa = []
 
 i = F32[2] //O valor atribuido ao i será usado posteriormente, não fazer modificações que vá excluir esse valor
 if (i > 0) {
-    LA23 = [0 , dA2y , dA2z],
-    LAB = [0 , 0 , dABz],
+    LM32 = [0 , dA2y , dA2z],
+    LMOa = [0 , 0 , dABz],
     console.log('O mancal que irá suportar o carregamento axial no eixo \'a\' é o esquerdo')
 }
 else {
-    LA23 = [0 ,dA2y , -(dABz - dA2z)], 
-    LAB = [0 , 0 , -dABz],
+    LM32 = [0 ,dA2y , -(dABz - dA2z)], 
+    LMOa = [0 , 0 , -dABz],
     console.log('O mancal que irá suportar o carregamento axial no eixo \'a\' é o direito')
 }
 
@@ -215,10 +158,19 @@ As próximas linhas desse programa foram feitas para calcular as reações do ma
 
 //LA23 x F32 + LAB x FB
 
-let pv3 = new Array(3) //mudar para pv1
-pv3[0] = (LA23[1]*F32[2]-LA23[2]*F32[1])
-pv3[1] = (LA23[2]*F32[0]-LA23[0]*F32[2])
-pv3[2] = (LA23[0]*F32[1]-LA23[1]*F32[0])
+let pv1 = new Array(3)
+pv1[0] = (LM32[1]*F32[2]-LM32[2]*F32[1])
+pv1[1] = (LM32[2]*F32[0]-LM32[0]*F32[2])
+pv1[2] = (LM32[0]*F32[1]-LM32[1]*F32[0])
+
+/* Não é possível fazer o produto vetorial de um vetor de incógnitas 
+com um vetor de números e continuar com o cálculo normalmente.
+Então: Como o vetor distância LDC sempre estará no eixo Z (não serão 
+considerados eixos inclinados), a resultante do produto vetorial entre 
+LBA e FB sempre será um vetor do tipo [-LBAz FBy, LBAk FBx, 0]. Assim, 
+o valor das componentes da reação em D será: Dx = (-pv1y - pv2y) / LBAz
+                                             Dy = (-pv1x - pv2x) / -LBAz
+*/
 
 // Determinação da força de reação no mancal sem carregamento axial
 
@@ -227,19 +179,19 @@ let FB = [0, 0, 0]
 
 //Esse if associa o valor da força F32z ao mancal que irá receber carga axial e define o valor da reação no mancal sem carga axial
 if (i > 0) {
-     FB[0] = (-pv3[1] / LAB[2]),
-     FB[1] = (-pv3[0] / -LAB[2])
+     FB[0] = (-pv1[1] / LMOa[2]),
+     FB[1] = (-pv1[0] / -LMOa[2])
 }
 else {
-    FA[0] = (-pv3[1]  / LAB[2]),
-    FA[1] = (-pv3[0]  / -LAB[2])
+    FA[0] = (-pv1[1]  / LMOa[2]),
+    FA[1] = (-pv1[0]  / -LMOa[2])
 }
 
 //Somatório das forças
 
 //F32 + FA + FB
 
-//Ainda usando o valor de i definido em (197) .Esse if pode ser usado junto ao da linha 229 futuraente, para reduzir o número de linhas de código.
+//Ainda usando o valor de i definido em (140) .Esse if pode ser usado junto ao da linha 194 futuraente, para reduzir o número de linhas de código.
 if (i>0) {
     FA[0] = -(F32[0]  + FB[0]),
     FA[1] = -(F32[1]  + FB[1]),
@@ -253,16 +205,23 @@ else {
 console.log('FA = ',FA)
 console.log('FB = ',FB)
 
+//Esse if determina a ordem que os termos da equação serão tratados,
+//Caso a força de A precise ser encontrada, ele isolará FA e vice-versa
+
+/* Nesse ponto temos o valor das forças aplicadas e forças de reação nos mancais*/
 
 //Cálculo dos momentos concentrados
 
-let LM32 = [0, di2/2, 0] //Distância do eixo ao ponto de aplicação da força
+let La32 = [0, di2/2, 0] //Distância do eixo ao ponto de aplicação da força
 
 //LM32 x F32 = M32
-let pm3 = new Array(3) //Mudar para pm1
-pm3[0] = (LM32[1]*F32[2]-LM32[2]*F32[1])
-pm3[1] = (LM32[2]*F32[0]-LM32[0]*F32[2])
-pm3[2] = (LM32[0]*F32[1]-LM32[1]*F32[0])
+let pm1 = new Array(3) //Mudar para pm1
+pm1[0] = (La32[1]*F32[2]-La32[2]*F32[1])
+pm1[1] = (La32[2]*F32[0]-La32[0]*F32[2])
+pm1[2] = (La32[0]*F32[1]-La32[1]*F32[0])
+
+//OBS: Os momentos concentrados em X irão entrar na análise do plano ZY,
+// e os de Y irão entrar na do plano ZX
 
 // Gráfico do momento fletor no eixo
 
@@ -274,10 +233,12 @@ FBx*i| + F32*(i-d1) + M32y
 Considerando que o eixo B é o da esquerda no inicio da questão
 */
 
-let D32B = Math.abs(dABz) - Math.abs(dA2z)
+let D32B = Math.abs(dABz) - Math.abs(dA2z) //Distâncias das forças no eixo Z
 let DBA = Math.abs(dABz)
 
 //Plano XZ
+
+//Esse for irá variar a distancia de 0,5 em 0,5in e calcular o momento em cada um desses pontos no eixo
 
 for (i = 0, MFx = [] ; i<=D32B;i = i+0.5){
     x = FB[0]*i
@@ -287,7 +248,7 @@ for (i = 0, MFx = [] ; i<=D32B;i = i+0.5){
 }
 
 for (i = D32B; i<=(DBA); i = i+0.5){
-    x = FB[0]*i + F32[0]*(i-D32B) + pm3[1] 
+    x = FB[0]*i + F32[0]*(i-D32B) + pm1[1] 
     MFx.push(x)
     //console.log(i)
     //console.log(MFx)
@@ -311,19 +272,17 @@ for (i = 0, MFy = [] ; i<=D32B;i = i+0.5){
 }
 
 for (i = D32B; i<=(DBA); i = i+0.5){
-    x = FB[1]*i + F32[1]*(i-D32B) + pm3[0] 
+    x = FB[1]*i + F32[1]*(i-D32B) + pm1[0] 
     MFy.push(x)
     //console.log(i)
     //console.log(MFy)
 }
-//Dessa forma, o vetor MFy tem todos os valores dos momentos no eixo Y
 
 let MmaxyA
-let MpyA = Math.max.apply(null, MFy)// retorna o maior valor positivo
-let MnyA = Math.min.apply(null, MFy)// retorna o maior valor negativo
-Math.abs(MpyA)>Math.abs(MnyA) ? MmaxyA = MpyA : MmaxyA = MnyA //compara qual o maior
-//console.log(MmaxyA)
-//Assim tem-se o maior momento no eixo Y.
+let MpyA = Math.max.apply(null, MFy)
+let MnyA = Math.min.apply(null, MFy)
+Math.abs(MpyA)>Math.abs(MnyA) ? MmaxyA = MpyA : MmaxyA = MnyA 
+
 
 //Determinação do maior momento fletor no eixo.
 
@@ -338,62 +297,48 @@ else{
 //Eixo b
 
 //Determinação do mancal com carregamento axial
-let LC23 = []
-let LC45 = []
-let LCD = []
+let LM23 = []
+let LM54 = []
+let LMOb = []
 
 i = F23[2] + F54[2]
 if (i > 0) {
-    LC23 = [0 , dC3y , dC3z],
-    LC45 = [0, dC4y , dC4z],
-    LCD = [0 , 0 , dCDz],
+    LM23 = [0 , dC3y , dC3z],
+    LM54 = [0, dC4y , dC4z],
+    LMOb = [0 , 0 , dCDz],
     console.log('O mancal que irá suportar o carregamento axial no eixo \'b\' é o esquerdo')
 }
 else {
-    LC23 = [0 ,dC3y , -(dCDz - dC3z)], 
-    LC45 = [0, dC4y , -(dCDz - dC4z)],
-    LCD = [0 , 0 , -dCDz],
+    LM23 = [0 ,dC3y , -(dCDz - dC3z)], 
+    LM54 = [0, dC4y , -(dCDz - dC4z)],
+    LMOb = [0 , 0 , -dCDz],
     console.log('O mancal que irá suportar o carregamento axial no eixo \'b\' é o direito')
 }
-//O sinal de - nessas expressões serve para inverter a direção do vetor,
-//caso contrário o vetor calculado seria o F23D por exemplo.
 
 //ANÁLISE DO MOMENTO
 
 //LC23 x F23 + LC45 x F54 + LCD x FD
 
-let pv1 = new Array(3)
-pv1[0] = (LC23[1]*F23[2]-LC23[2]*F23[1])
-pv1[1] = (LC23[2]*F23[0]-LC23[0]*F23[2])
-pv1[2] = (LC23[0]*F23[1]-LC23[1]*F23[0])
-
 let pv2 = new Array(3)
-pv2[0] = (LC45[1]*F54[2]-LC45[2]*F54[1])
-pv2[1] = (LC45[2]*F54[0]-LC45[0]*F54[2])
-pv2[2] = (LC45[0]*F54[1]-LC45[1]*F54[0])
+pv2[0] = (LM23[1]*F23[2]-LM23[2]*F23[1])
+pv2[1] = (LM23[2]*F23[0]-LM23[0]*F23[2])
+pv2[2] = (LM23[0]*F23[1]-LM23[1]*F23[0])
 
-/* Não é possível fazer o produto vetorial de um vetor de incógnitas 
-com um vetor de números e continuar com o cálculo normalmente.
-Então: Como o vetor distância LDC sempre estará no eixo Z (não serão 
-considerados eixos inclinados), a resultante do produto vetorial entre 
-LDC e FD sempre será um vetor do tipo [-LDCz FDy, LDCk FDx, 0]. Assim, 
-o valor das componentes da reação em D será: Dx = (-pv1y - pv2y) / LCDz
-                                             Dy = (-pv1x - pv2x) / -LCDz
-*/
-
-//OBS: A primeira reação a ser calculada é a do mancal sem carga axial, 
-//então o vetor diatância a ser usado deve ser a do mancal com carregamento axial
+let pv3 = new Array(3)
+pv3[0] = (LM54[1]*F54[2]-LM54[2]*F54[1])
+pv3[1] = (LM54[2]*F54[0]-LM54[0]*F54[2])
+pv3[2] = (LM54[0]*F54[1]-LM54[1]*F54[0])
 
 let FC = [0, 0, 0]
 let FD = [0, 0, 0]
 
 if (i > 0) {
-     FD[0] = ((-pv1[1] - pv2[1]) / LCD[2]),
-     FD[1] = ((-pv1[0] - pv2[0]) / -LCD[2])
+     FD[0] = ((-pv2[1] - pv3[1]) / LMOb[2]),
+     FD[1] = ((-pv2[0] - pv3[0]) / -LMOb[2])
 }
 else {
-    FC[0] = ((-pv1[1] - pv2[1]) / LCD[2]),
-    FC[1] = ((-pv1[0] - pv2[0]) / -LCD[2])
+    FC[0] = ((-pv2[1] - pv3[1]) / LMOb[2]),
+    FC[1] = ((-pv2[0] - pv3[0]) / -LMOb[2])
 }
 
 //Somatório das forças
@@ -413,35 +358,22 @@ else {
 console.log('FC = ',FC)
 console.log('FD = ',FD)
 
-//Esse if determina a ordem que os termos da equação serão tratados,
-//Caso a força de C precise ser encontrada, ele isolará FC e vice-versa
-
-/* Nesse ponto temos o valor das forças aplicadas e forças de reação nos mancais*/
-
-
 //Cálculo dos momentos concentrados
 
-let LM23 = [0, -di3/2, 0] //Distância do eixo ao ponto de aplicação da força
-let LM54 = [0, di4/2, 0]
-
-/*OBS: Deve ser feita alguma alteração posterior para determinar o sinal 
-desses termos de forma automática*/
+let Lb23 = [0, -di3/2, 0] 
+let Lb54 = [0, di4/2, 0]
 
 //LM23 x F23 = M23
-let pm1 = new Array(3)
-pm1[0] = (LM23[1]*F23[2]-LM23[2]*F23[1])
-pm1[1] = (LM23[2]*F23[0]-LM23[0]*F23[2])
-pm1[2] = (LM23[0]*F23[1]-LM23[1]*F23[0])
+let pm2 = new Array(3)
+pm2[0] = (Lb23[1]*F23[2]-Lb23[2]*F23[1])
+pm2[1] = (Lb23[2]*F23[0]-Lb23[0]*F23[2])
+pm2[2] = (Lb23[0]*F23[1]-Lb23[1]*F23[0])
 
 //LM45 X F54 = M45
-let pm2 = new Array(3)
-pm2[0] = (LM54[1]*F54[2]-LM54[2]*F54[1])
-pm2[1] = (LM54[2]*F54[0]-LM54[0]*F54[2])
-pm2[2] = (LM54[0]*F54[1]-LM54[1]*F54[0])
-
-//OBS: Os momentos concentrados em X irão entrar na análise do plano ZY,
-// e os de Y irão entrar na do plano ZX
-
+let pm3 = new Array(3)
+pm3[0] = (Lb54[1]*F54[2]-Lb54[2]*F54[1])
+pm3[1] = (Lb54[2]*F54[0]-Lb54[0]*F54[2])
+pm3[2] = (Lb54[0]*F54[1]-Lb54[1]*F54[0])
 
 // Gráfico do momento fletor no eixo
 
@@ -467,28 +399,24 @@ for (i = 0, MFx = [] ; i<=D23D;i = i+0.5){
 }
 
 for (i = D23D; i<=(D23D+D2345); i = i+0.5){
-    x = FD[0]*i + F23[0]*(i-D23D) + pm1[1] 
+    x = FD[0]*i + F23[0]*(i-D23D) + pm2[1] 
     MFx.push(x)
     //console.log(i)
     //console.log(MFx)
 }
 
 for (i = (D23D+D2345); i<=(D23D+D2345+DC45); i = i+0.5){
-    x = FD[0]*i + F23[0]*(i-D23D) + pm1[1] + F54[0]*(i-(D23D+D2345)) + pm2[1] 
+    x = FD[0]*i + F23[0]*(i-D23D) + pm2[1] + F54[0]*(i-(D23D+D2345)) + pm3[1] 
     MFx.push(x)
     //console.log(i)
     //console.log(MFx)
 }
-//Dessa forma, o vetor MFx tem todos os valores dos momentos no eixo X
 
 let MmaxxB
-let MpxB = Math.max.apply(null, MFx)// retorna o maior valor positivo
-let MnxB = Math.min.apply(null, MFx)// retorna o maior valor negativo
-Math.abs(MpxB)>Math.abs(MnxB) ? MmaxxB = MpxB : MmaxxB = MnxB //compara qual o maior
+let MpxB = Math.max.apply(null, MFx)
+let MnxB = Math.min.apply(null, MFx)
+Math.abs(MpxB)>Math.abs(MnxB) ? MmaxxB = MpxB : MmaxxB = MnxB 
 //console.log(Mmaxx)
-
-//Assim tem-se o maior momento no eixo X.
-
 
 //Plano YZ
 
@@ -500,27 +428,24 @@ for (i = 0, MFy = [] ; i<=D23D;i = i+0.5){
 }
 
 for (i = D23D; i<=(D23D+D2345); i = i+0.5){
-    x = FD[1]*i + F23[1]*(i-D23D) + pm1[0] 
+    x = FD[1]*i + F23[1]*(i-D23D) + pm2[0] 
     MFy.push(x)
     //console.log(i)
     //console.log(MFy)
 }
 
 for (i = (D23D+D2345); i<=(D23D+D2345+DC45); i = i+0.5){
-    x = FD[1]*i + F23[1]*(i-D23D) + pm1[0] + F54[1]*(i-(D23D+D2345)) + pm2[0] 
+    x = FD[1]*i + F23[1]*(i-D23D) + pm2[0] + F54[1]*(i-(D23D+D2345)) + pm3[0] 
     MFy.push(x)
     //console.log(i)
     //console.log(MFy)
 }
-//Dessa forma, o vetor MFy tem todos os valores dos momentos no eixo y
 
 let MmaxyB
-let MpyB = Math.max.apply(null, MFy)// retorna o maior valor positivo
-let MnyB = Math.min.apply(null, MFy)// retorna o maior valor negativo
-Math.abs(MpyB)>Math.abs(MnyB) ? MmaxyB = MpyB : MmaxyB = MnyB //compara qual o maior
+let MpyB = Math.max.apply(null, MFy)
+let MnyB = Math.min.apply(null, MFy)
+Math.abs(MpyB)>Math.abs(MnyB) ? MmaxyB = MpyB : MmaxyB = MnyB 
 //console.log(MmaxyB)
-
-//Assim tem-se o maior momento no eixo Y.
 
 //Determinação do maior momento fletor no eixo.
 
@@ -536,18 +461,18 @@ else{
 //Eixo c
 
 //Determinação do mancal com carregamento axial
-let LE45 = []
-let LEF = []
+let LM45 = []
+let LMOc = []
 
 i = F45[2]
 if (i > 0) {
-    LE45 = [0 , dE5y , dE5z],
-    LEF = [0 , 0 , dEFz],
+    LM45 = [0 , dE5y , dE5z],
+    LMOc = [0 , 0 , dEFz],
     console.log('O mancal que irá suportar o carregamento axial no eixo \'c\' é o esquerdo')
 }
 else {
-    LE45 = [0 ,dE5y , -(dEFz - dE5z)], 
-    LEF = [0 , 0 , -dEFz],
+    LM45 = [0 ,dE5y , -(dEFz - dE5z)], 
+    LMOc = [0 , 0 , -dEFz],
     console.log('O mancal que irá suportar o carregamento axial no eixo \'c\' é o direito')
 }
 
@@ -556,9 +481,9 @@ else {
 //LE45 x F45 + LEF x FF
 
 let pv4 = new Array(3)
-pv4[0] = (LE45[1]*F45[2]-LE45[2]*F45[1])
-pv4[1] = (LE45[2]*F45[0]-LE45[0]*F45[2])
-pv4[2] = (LE45[0]*F45[1]-LE45[1]*F45[0])
+pv4[0] = (LM45[1]*F45[2]-LM45[2]*F45[1])
+pv4[1] = (LM45[2]*F45[0]-LM45[0]*F45[2])
+pv4[2] = (LM45[0]*F45[1]-LM45[1]*F45[0])
 
 // Determinação da força de reação no mancal sem carregamento axial
 
@@ -566,12 +491,12 @@ let FE = [0, 0, 0]
 let FF = [0, 0, 0]
 
 if (i > 0) {
-     FF[0] = (-pv4[1] / LEF[2]),
-     FF[1] = (-pv4[0] / -LEF[2])
+     FF[0] = (-pv4[1] / LMOc[2]),
+     FF[1] = (-pv4[0] / -LMOc[2])
 }
 else {
-    FE[0] = (-pv4[1]  / LEF[2]),
-    FE[1] = (-pv4[0]  / -LEF[2])
+    FE[0] = (-pv4[1]  / LMOc[2]),
+    FE[1] = (-pv4[0]  / -LMOc[2])
 }
 
 //Somatório das forças
@@ -593,13 +518,13 @@ console.log('FF = ',FF)
 
 //Cálculo dos momentos concentrados
 
-let LM45 = [0, -di5/2, 0] //Distância do eixo ao ponto de aplicação da força
+let Lc45 = [0, -di5/2, 0] 
 
 //LM32 x F32 = M32
 let pm4 = new Array(3)
-pm4[0] = (LM45[1]*F45[2]-LM45[2]*F45[1])
-pm4[1] = (LM45[2]*F45[0]-LM45[0]*F45[2])
-pm4[2] = (LM45[0]*F45[1]-LM45[1]*F45[0])
+pm4[0] = (Lc45[1]*F45[2]-Lc45[2]*F45[1])
+pm4[1] = (Lc45[2]*F45[0]-Lc45[0]*F45[2])
+pm4[2] = (Lc45[0]*F45[1]-Lc45[1]*F45[0])
 
 // Gráfico do momento fletor no eixo
 
@@ -629,14 +554,12 @@ for (i = D45F; i<=(DFE); i = i+0.5){
     //console.log(i)
     //console.log(MFx)
 }
-//Dessa forma, o vetor MFx tem todos os valores dos momentos no eixo X
 
 let MmaxxC
-let MpxC = Math.max.apply(null, MFx)// retorna o maior valor positivo
-let MnxC = Math.min.apply(null, MFx)// retorna o maior valor negativo
-Math.abs(MpxC)>Math.abs(MnxC) ? MmaxxC = MpxC : MmaxxC = MnxC //compara qual o maior
+let MpxC = Math.max.apply(null, MFx)
+let MnxC = Math.min.apply(null, MFx)
+Math.abs(MpxC)>Math.abs(MnxC) ? MmaxxC = MpxC : MmaxxC = MnxC 
 //console.log(MmaxxC)
-//Assim tem-se o maior momento no eixo X.
 
 //Plano YZ
 
@@ -653,14 +576,12 @@ for (i = D45F; i<=(DFE); i = i+0.5){
     //console.log(i)
     //console.log(MFy)
 }
-//Dessa forma, o vetor MFy tem todos os valores dos momentos no eixo Y
 
 let MmaxyC
-let MpyC = Math.max.apply(null, MFy)// retorna o maior valor positivo
-let MnyC = Math.min.apply(null, MFy)// retorna o maior valor negativo
-Math.abs(MpyC)>Math.abs(MnyC) ? MmaxyC = MpyC : MmaxyC = MnyC //compara qual o maior
+let MpyC = Math.max.apply(null, MFy)
+let MnyC = Math.min.apply(null, MFy)
+Math.abs(MpyC)>Math.abs(MnyC) ? MmaxyC = MpyC : MmaxyC = MnyC 
 //console.log(MmaxyAC)
-//Assim tem-se o maior momento no eixo Y.
 
 //Determinação do maior momento fletor no eixo.
 
@@ -673,12 +594,10 @@ else{
 
 
 /*OBS 
-mudar nome do vetor distancia do mancal de LA etc para LM... (197,)
-mudar distância do eixo para o ponto de aplicação de LM para La,Lb,Lc etc...(259,)
 (121) estabelecer método para determinar a engrenagem motora do sistema e analisar os impactos disso no memsmo
 (138) deve ser adicionado um if na definição do sentido da força axial para, caso a engrenagem usada tenha dentes retos, ela seja igual a zero
 (158) fazer um if para determinar qual engrenagem será a motora no restante dos sistemas a partir de apenas uma
 (169) essas linhas de código podem ser substituídas usando o mesmo if da linha 158, assim ao determinar qual será a engrenagem motora do par e o sentido de rotação,
 já será determinado qual o sentdo da força axial, se ela existir.
+Checar sinais do vetor distância do eixo à força
 */
-//(276)
